f6e472a15de3f2fcb7b7b44db68ffca956d5c31e
kap <william.kapke@gmail.com>
2024-08-26 11:05:50 -0700

    bug fixes

---
+++ assets/Pectabs-XhQOvPj3.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,a as c,b as e}from"./index-CfTp870n.js";const o={},a={class:"p-6"},_=e("h2",{class:"text-2xl font-bold mb-4"},"Pectabs",-1),n=e("p",null,"....",-1),r=[_,n];function l(d,i){return c(),s("article",a,r)}const h=t(o,[["render",l]]);export{h as default};
